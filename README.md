4ed38ffc12f01cd0e454ad2d8312b530ed96bfaa
Rednasrot <Rednasrot@gmail.com>
2024-11-22 09:55:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lekse19.11.2024
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Rednasrot/lekse19.11.2024)
---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Hello!');
+
+const person = {
+  firstName: 'Tor',
+  lastName: ' Bruvik',
+  age: 25,
+};
+console.log(person.firstName + person.lastName + ' is ' + person.age);
+
+let x = 5;
+let y = 10;
+let m = x + y;
+alert(m);
+
+console.log(m);
